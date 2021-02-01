7d0b03d88755d0e94aa58d806f76377ad67e1828
ibizawares <43162466+ibizawares@users.noreply.github.com>
2021-01-31 19:33:28 -0600

    Update and rename samu.js to ibizawares.js

--- plugins/ibizawares.js
+++ plugins/ibizawares.js
@@ -1,16 +1,16 @@
 let handler = async m => m.reply(`
-╭─「 Smu330 • APOYAME:) 」
+╭─「 Ibizawares • APOYAME:) 」
 │ • My canal de youtube:
-│ • [https://www.youtube.com/channel/UCqPXxG2ZdDe_ugOqMHDRMqg]
+│ • [https://youtube.com/channel/UChAGpQT2Upc-5m1p9x02djg]
 │ 
 │ • My Facebook:
-│ • [https://www.facebook.com/tupapi.samu330]
+│ • [https://www.facebook.com/agus.ochoa.94]
 │
 │ • My grupo de Whatsapp:
-│ • [https://chat.whatsapp.com/ELeHAmX3P6j1xy1qNRjEXR]
+│ • [https://chat.whatsapp.com/KOh7MTJ1tnS9TvvJG3iGbG]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
-handler.help = ['samu']
+handler.help = ['ibizawares']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
 
